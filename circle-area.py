c5c77bc01f7503a7965463a5885d9b5636abe5ed
James Shaw <js102@zepler.net>
2014-08-25 19:26:55 +0100

    read radius from command line argument

--- circle-area.py
+++ circle-area.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 from math import *
+import sys
+
+radius = float(sys.argv[1])
 
 print("The area of a circle with radius {r} is {area}" \
-	.format(r=3, area=pi*3**2))
+	.format(r=radius, area=pi*radius**2))
