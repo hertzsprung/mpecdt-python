22b620981fc1d5454f8b10c07ad12f7ead0eeb44
James Shaw <js102@zepler.net>
2014-08-27 20:40:31 +0100

    add lecture notes on functions

--- circle-area.py
+++ circle-area.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 from math import *
-import sys
 
-radius = float(sys.stdin.readline())
+def circle_area(radius):
+	return pi*radius**2
 
-print("The area of a circle with radius {r} is {area:.4g}" \
-	.format(r=radius, area=pi*radius**2))
+def print_circle_area(radius, area):
+	print("The area of a circle " + \
+	"with radius {r} is {area:.4g}" \
+	.format(r=radius, area=area))
+
+radius = 4.5
+area = circle_area(radius)
+print_circle_area(radius, area)
