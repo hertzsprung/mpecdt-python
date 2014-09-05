e348cfbebe99fddb8d734e9fc77c23bc407c38e2
James Shaw <js102@zepler.net>
2014-09-05 13:57:51 +0100

    add netcdf chapter

--- netcdf_plot.py
+++ netcdf_plot.py
@@ -4,21 +4,23 @@ from mpl_toolkits.basemap import Basemap, shiftgrid
 import matplotlib.pyplot as plt
 import numpy as np
 
-era40 = Dataset('ECMWF_ERA-40_subset.nc', mode='r')
-time = era40.variables['time'][:]
-longitudes = era40.variables['longitude'][:]
-latitudes = era40.variables['latitude'][:]
-u = era40.variables['p10u'][0][:]
+def load_era40(filename):
+    era40 = Dataset(filename, mode='r')
+    longitudes = era40.variables['longitude'][:]
+    latitudes = era40.variables['latitude'][:]
+    t = era40.variables['p2t'][0][:]
+    era40.close()
+    return longitudes, latitudes, t
 
-era40.close()
+longitudes, latitudes, t = load_era40('ECMWF_ERA-40_subset.nc')
 
-basemap = Basemap(projection='merc', llcrnrlat=-80,urcrnrlat=80,\
-                    llcrnrlon=-180,urcrnrlon=180,lat_ts=20,resolution='c')
-u, longitudes = shiftgrid(180.0, u, longitudes, start=False)
+basemap = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, \
+    llcrnrlon=-180, urcrnrlon=180, lat_ts=20, resolution='c')
+t, longitudes = shiftgrid(180.0, t, longitudes, start=False)
 lon, lat = np.meshgrid(longitudes, latitudes)
 xi, yi = basemap(lon, lat)
 
-basemap.pcolor(xi, yi, u)
+basemap.pcolor(xi, yi, t)
 basemap.drawcoastlines()
 
 plt.show()
